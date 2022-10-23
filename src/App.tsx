e6a1782ff67b06503038a83bbda7789f9c8f0cc7
Natalia Minenkova <nminiankova@gmail.com>
2022-10-23 13:50:47 +0300

    add branch

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+
+export const App = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ullam suscipit eaque ea corrupti dolore asperiores officia laborum voluptas? Incidunt tempore quasi rerum quo aut delectus accusantium eum laudantium provident.</div>
+  )
+}
+
+
+
