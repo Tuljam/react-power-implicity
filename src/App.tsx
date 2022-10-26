0bb121d8341b78583832362a9574c21a0291cc1f
Natalia Minenkova <nminiankova@gmail.com>
2022-10-26 11:55:16 +0300

    add styles for nav&slide

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { Navbar } from "./components/Navbar"
-import { Slider } from "./components/Slider"
+import './app.scss';
+import { Navbar } from './components/Nav/Navbar';
+import { Slider } from './components/Slider/Slider';
+
+
 
 
 export const App = () => {
   return (
     <div className="wrapper">
+    
       <Navbar/>
       <Slider/>
     </div>
   )
 }
+ export default App;
 
 
 
