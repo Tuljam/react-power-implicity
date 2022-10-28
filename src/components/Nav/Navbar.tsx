0832a19d928fbb557e7171dd2f396379492ff183
Natalia Minenkova <nminiankova@gmail.com>
2022-10-28 12:42:41 +0300

    correct styles for wrapper

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -9,18 +9,18 @@ export const Navbar = () => {
         <LogoIcon />
       </div>
       <nav className="navbar__navigation">
-        <a href=" " className="navbar__navigation-link">
+        <a href="#" className="navbar__navigation-link">
           Features
         </a>
-        <a href=" " className="navbar__navigation-link">
+        <a href="#" className="navbar__navigation-link">
           Partners
         </a>
-        <a href=" " className="navbar__navigation-link">
+        <a href="#" className="navbar__navigation-link">
           Stories
         </a>
       </nav>
       <button className="navbar__button">
-        <span>Download for free</span>
+        <span className="navbar__batton-text">Download for free</span>
       </button>
       <button className="navbar__burger">
         <Burger />
